684cb15906ef5b517de5df1f483d90f0be491fc5
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-15 18:44:10 +0630

    Add layout

--- src/components/Main/CardsContainer.js
+++ src/components/Main/CardsContainer.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Card from './Card';
+import uniqid from 'uniqid';
 
 const CardsContainer = (props) => {
   const { cards, onCardClick } = props;
 
   return (
-    <div>
+    <div className='grid grid-cols-2 md:grid-cols-4 justify-center gap-7 mt-6'>
       {cards.map((card) => (
         <Card
-          name={card.agentName}
+          name={card.displayName}
           imgSrc={card.displayIcon}
           onClick={onCardClick}
+          key={uniqid()}
         />
       ))}
     </div>
