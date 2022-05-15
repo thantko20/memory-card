734a3945572b464a89d0412b4b1314bbdff4330f
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-15 20:15:15 +0630

    Add logic for populating the cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import _ from 'lodash';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
 const App = () => {
+  // Current Cards Array size
+  const [size, setSize] = useState(4);
+
   // Original Data
   const [agentsData, setAgentsData] = useState([]);
 
@@ -19,16 +23,25 @@ const App = () => {
     fetchAgentsData();
   }, []);
 
+  // Cards
+  const [playCards, setPlayCards] = useState([]);
+  // playCards array will be updated as the size changes
+  // with level
+  useEffect(() => {
+    setPlayCards(_.sampleSize(agentsData, size));
+  }, [agentsData, size]);
+
   return (
     <div className='min-h-screen bg-zinc-900 text-slate-50 font-varela'>
       <Header />
       <Main
-        cards={agentsData}
+        cards={playCards}
         handleOnCardClick={() => {}}
         level={'1'}
         score={'2'}
         highScore={'3'}
       />
+      <button onClick={() => setSize(size + 1)}>Click</button>
     </div>
   );
 };
