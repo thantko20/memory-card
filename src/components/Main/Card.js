31c3e18845cf9a60fedfbdb6d3c9850a8e2c4167
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-15 18:43:47 +0630

    Style the card

--- src/components/Main/Card.js
+++ src/components/Main/Card.js
@@ -4,9 +4,12 @@ const Card = (props) => {
   const { name, imgSrc, onClick } = props;
 
   return (
-    <div onClick={() => onClick(name)}>
-      <img src={imgSrc} alt={`${name}`} />
-      <div>{name}</div>
+    <div
+      onClick={() => onClick(name)}
+      className='bg-gradient-to-r from-red-200 via-purple-500 to-rose-500 w-[150px] bg-slate-700 text-center rounded-md overflow-hidden transition duration-500 cursor-pointer hover:translate-y-[-8px] hover:shadow-md hover:shadow-slate-500'
+    >
+      <img src={imgSrc} alt={`${name}`} className='w-full aspect-square' />
+      <div className='bg-rose-50 text-rose-800 font-bold text-sm'>{name}</div>
     </div>
   );
 };
