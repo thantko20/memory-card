812cf1c4cf7ba401cf6af02abec4123cea13e77c
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-16 09:55:37 +0630

    Add background gradient

--- src/components/Main/Card.js
+++ src/components/Main/Card.js
@@ -6,10 +6,12 @@ const Card = (props) => {
   return (
     <div
       onClick={() => onClick(name)}
-      className='bg-gradient-to-r from-red-200 via-purple-500 to-rose-500 w-[150px] bg-slate-700 text-center rounded-md overflow-hidden transition duration-500 cursor-pointer hover:translate-y-[-8px] hover:shadow-md hover:shadow-slate-500'
+      className='bg-gradient-to-r from-red-200 via-purple-500 to-rose-500 w-[150px] bg-slate-700 text-center rounded-md overflow-hidden transition duration-500 cursor-pointer hover:translate-y-[-8px] hover:scale-105'
     >
       <img src={imgSrc} alt={`${name}`} className='w-full aspect-square' />
-      <div className='bg-rose-50 text-rose-800 font-bold text-sm'>{name}</div>
+      <div className='bg-white opacity-50 text-red-900 font-bold text-sm'>
+        {name}
+      </div>
     </div>
   );
 };
