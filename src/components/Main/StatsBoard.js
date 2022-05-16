06fde10ec6f8b1016786a3d98efb6b4792403b36
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-16 10:02:16 +0630

    Add some colours

--- src/components/Main/StatsBoard.js
+++ src/components/Main/StatsBoard.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
 const StatsBoard = (props) => {
-  const { level, score, highestScore } = props;
+  const { level, score, highScore } = props;
 
   return (
-    <div>
-      <h2>Level: {level}</h2>
+    <div className='flex items-center gap-4'>
+      <h2>
+        Level: <span className='font-bold text-lg text-red-400'>{level}</span>
+      </h2>
       <div>
-        <div>Score: {score}</div>
-        <div>Highest Score: {highestScore}</div>
+        <div>
+          Score:{' '}
+          <span className='font-bold text-lg text-indigo-400'>{score}</span>
+        </div>
+        <div>
+          High Score:{' '}
+          <span className='font-bold text-lg text-emerald-400'>
+            {highScore}
+          </span>
+        </div>
       </div>
     </div>
   );
