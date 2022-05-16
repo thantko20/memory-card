1965679ae0f18d8452057632029592e69ad3e824
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-16 11:40:51 +0630

    Clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
+    fontFamily: {
+      varela: ['Varela Round', 'sans-serif'],
+    },
+    gridTemplateColumns: {
+      2: 'repeat(2, 150px)',
+      4: 'repeat(4, 150px)',
+    },
   },
   plugins: [],
 };
